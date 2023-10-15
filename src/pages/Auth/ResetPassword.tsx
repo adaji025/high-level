a4ccc37645004b272f40817590e8ece9f090176b
Adaji Mukhtar <53816067+adaji025@users.noreply.github.com>
2023-10-15 12:01:50 +0100

    made adjustment to the auth pages

--- src/pages/Auth/ResetPassword.tsx
+++ src/pages/Auth/ResetPassword.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import useNotification from "../../hooks/useNotification";
 import { resetPassword } from "../../services/auth";
 import { showNotification } from "@mantine/notifications";
+import HighLevel from "../../assets/svgs/high-level.svg";
 
 const ResetPassword = () => {
   const [loading, setLoading] = useState(false);
@@ -45,39 +46,33 @@ const ResetPassword = () => {
     <>
       <LoadingOverlay visible={loading} />
       <div className="min-h-screen w-full flex flex-col justify-center items-center">
-          <h1 className="text-center text-[24px] lg:text-[30px] font-semibold text-mainText">
-            Create new password
-          </h1>
-          <h3 className="text-center font-medium mt-2 text-secondaryText/50">
-            Must be at least 8 characters.
-          </h3>
+        <div className="my-10">
+          <img src={HighLevel} alt="high level" />
+        </div>
+        <h1 className="text-center text-[24px] lg:text-[30px] font-semibold text-mainText">
+          Create new password
+        </h1>
+        <h3 className="text-center font-medium mt-2 text-secondaryText/50">
+          Must be at least 8 characters.
+        </h3>
         <div className="max-w-[420px] w-full p-[20px]">
           <form onSubmit={form.onSubmit((values) => submit(values))}>
             <Box mt={16}>
               <PinInput size="md" {...form.getInputProps("otp")} />
 
-              <Box>
-                <PasswordInput
-                  mt={24}
-                  size="md"
-                  placeholder="Choose a password"
-                  {...form.getInputProps("newPassword")}
-                />
-                <span className="text-xs text-[#344054]">
-                  Must be at least 8 characters.
-                </span>
-              </Box>
-              <Box>
-                <PasswordInput
-                  mt={24}
-                  size="md"
-                  placeholder="Confirm password"
-                  {...form.getInputProps("confirmPassword")}
-                />
-                <span className="text-xs text-[#344054]">
-                  Must be at least 8 characters.
-                </span>
-              </Box>
+              <PasswordInput
+                mt={24}
+                size="md"
+                placeholder="Choose a password"
+                {...form.getInputProps("newPassword")}
+              />
+
+              <PasswordInput
+                mt={24}
+                size="md"
+                placeholder="Confirm password"
+                {...form.getInputProps("confirmPassword")}
+              />
 
               <button className="mt-6 w-full bg-highLevelRed text-white p-3 rounded-lg font-bold">
                 Get started
